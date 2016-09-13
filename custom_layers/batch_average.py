b4a27dbe5f4224a6cb5e5cecb56ecf21425b1895
= <=>
2016-09-14 01:17:49 +0200

    Some changes:
    - Corrected small bugs when calling the class (self.)
    - If the weights do not exist, they are automatically downloaded
    - Solved some import issues when batch-averaging
    - The process now starts and finishes showing the predicted classes

--- custom_layers/batch_average.py
+++ custom_layers/batch_average.py
@@ -1,15 +1,15 @@
 import lasagne
 import lasagne.layers
-import lasagne.layers.Layer
 import numpy as np
-
+import theano
+import theano.tensor as T
 
 class BatchAverageLayer(lasagne.layers.Layer):
     def __init__(self, incoming, frames_per_video, **kwargs):
-        super(DotLayer, self).__init__(incoming, **kwargs)
+        super(BatchAverageLayer, self).__init__(incoming, **kwargs)
         num_inputs = self.input_shape[1]
         # The shape of the matrix will be < batch_size / frames_per_video , batch_size >
-        constant_batch_mapper = np.zeros([self.input_shape[0] / frames_per_video, self.input_shape[0]])
+        constant_batch_mapper = np.zeros([self.input_shape[0] / frames_per_video, self.input_shape[0]], dtype = np.float32)
         for i in range(self.input_shape[0] / frames_per_video):
         	# for each video we write the same weights here, so the result of multiplying this will be equivalent to an average
         	for j in range(frames_per_video):
--- network/TestNetwork.py
+++ network/TestNetwork.py
@@ -1,14 +1,14 @@
-from network.VGG16 import VGG16Network
+from network.base_networks.googlenet import GooglenetNetwork
 import lasagne
 from lasagne.layers import set_all_param_values
 import pickle
 import numpy as np
 import cv2
-from custom_layers import BatchAverageLayer
+from custom_layers.batch_average import BatchAverageLayer
 
-def loadImage(imagepath):
+def loadImage(imagepath, mean_image):
     img = cv2.resize(cv2.imread(imagepath), (224, 224))
-    mean_pixel = MEAN_IMAGE # [103.939, 116.779, 123.68]
+    mean_pixel = mean_image # [103.939, 116.779, 123.68]
     img = img.astype(np.float32, copy=False)
     for c in range(3):
         img[:, :, c] = img[:, :, c] - mean_pixel[c]
@@ -16,20 +16,30 @@ def loadImage(imagepath):
     img = np.expand_dims(img, axis=0)
     return img
 
+print("starting")
+batch_size = 12
+frames_per_video = 6
 
-frames_per_video = 12
-
-net = VGG16Network()
+print("defining and loading network")
+net = GooglenetNetwork(batch_size)
 net, classes, mean_image = net.get_network()
-net['aggregate_per_video'] = BatchAverageLayer(net['fc8'], frames_per_video = frames_per_video)
 
-cat_image = loadImage('../../Desktop/cat.jpg')
-bed_image = loadImage('../../Desktop/bed.jpg')
+print("adding my own layer")
+net['aggregate_per_video'] = BatchAverageLayer(net['loss3/classifier'], frames_per_video = frames_per_video)
+
+output_layer = net['aggregate_per_video']
+
+print("preparing input...")
+cat_image = loadImage('../../Desktop/cat.jpg', mean_image)
+bed_image = loadImage('../../Desktop/bed.jpg', mean_image)
 
 data_to_feed = np.vstack([cat_image]*frames_per_video + [bed_image]*frames_per_video)
+
+print("feeding input...")
 
 classification_result = lasagne.layers.get_output(output_layer, data_to_feed)
 
 print(data_to_feed.shape)
-print(classification_result.shape)
+print(classification_result.eval().shape)
 
+print(map(lambda x: classes[x], classification_result.eval().argmax(axis=1)))
