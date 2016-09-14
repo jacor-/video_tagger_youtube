b81f0f5b62e6757f2cef24bce9a59410dabb3b9a
unknown <Jose.Cordero@BARLTW1047.midasplayer.local>
2016-09-15 01:29:45 +0200

    ...

--- scripts/youtube_apis.py
+++ scripts/youtube_apis.py
@@ -1,61 +1,40 @@
 
 #!/usr/bin/python
 
-from apiclient.discovery import build
-from apiclient.errors import HttpError
-from oauth2client.tools import argparser
+import urllib
+import requests
+
+api_key = "AIzaSyDV_m-yM9Fdba2rem6w6Cy2GJeWwE2_3r8"
+search_criteria = ["dog", "cat", "bird", "turtle", 
+                   "fight", "run", "walk", "climb", 
+                   "messi", "neymar", "ronaldo", "guardiola", "mourinho",
+                   "barcelona", "london", "rome", "amsterdam", "europe", "world",
+                   "plane", "car", "bike",
+                   "mariano rajoy", "pablo iglesias", "pedro sanchez", "albert rivera"
+                   ]
+
+def collect_videos(search_criteria):
+  p_search_criteria = urllib.quote(search_criteria)
+  req = requests.get("https://www.googleapis.com/youtube/v3/search?key={key}&fields=items(id(videoId))&part=id,snippet&q={query}&maxResults=50".format(query = p_search_criteria, key = api_key))
+  retrieved_videos = eval(req.content)
+  videos, tags = [], []
+  for item in retrieved_videos['items']:
+      videoid = item['id']['videoId']
+      req = requests.get("https://www.googleapis.com/youtube/v3/videos?key={key}&fields=items(snippet(tags))&part=snippet&id={video_id}".format(video_id = videoid, key = api_key))
+      try:
+        tag = eval(req.content)['items'][0]['snippet']['tags']
+      except:
+        tag = []
+      tags.append(list(tag))
+      videos.append(videoid)
+  return videos, tags
 
 
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyDV_m-yM9Fdba2rem6w6Cy2GJeWwE2_3r8"
+print("Expected " + str(len(search_criteria) * 50) + " videos")
 
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
-
-def youtube_search(options):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=options.q,
-    part="id,snippet(title,description,tags)",
-    maxResults=options.max_results
-  ).execute()
-
-  videos = []
-  channels = []
-  playlists = []
-
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get("items", []):
-    print(search_result['snippet'].keys())
-    if search_result["id"]["kind"] == "youtube#video":
-      videos.append("%s (%s)" % (search_result["snippet"]["title"],
-                                      search_result["id"]["videoId"]))
-    elif search_result["id"]["kind"] == "youtube#channel":
-      channels.append("%s (%s)" % (search_result["snippet"]["title"],
-                                   search_result["id"]["channelId"]))
-    elif search_result["id"]["kind"] == "youtube#playlist":
-      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-                                    search_result["id"]["playlistId"]))
-  print "Videos:\n", "\n".join(videos), "\n"
-  print "Channels:\n", "\n".join(channels), "\n"
-  print "Playlists:\n", "\n".join(playlists), "\n"
-
-
-if __name__ == "__main__":
-  print("perro")
-  argparser.add_argument("--q", help="Search term", default="Star Wars")
-  print("perro2")
-  argparser.add_argument("--max-results", help="Max results", default=25)
-  args = argparser.parse_args()
-
-  try:
-    youtube_search(args)
-  except HttpError, e:
-    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+vid_id, tags = [],[]
+for to_search in search_criteria:
+  videos, tag = collect_videos(to_search)
+  vid_id.append(videos)
+  tags.append(tag)
+  break
