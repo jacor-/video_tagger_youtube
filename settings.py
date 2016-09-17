2d0a1e241c2bc1d6b48e6873036c2be862b66b62
= <=>
2016-09-17 18:14:13 +0200

    Improved youtube video downloader. It also prepared the tags. THis part must be improved

--- settings.py
+++ settings.py
@@ -2,3 +2,7 @@
 datapath = '../data'
 path_pretrained_weights = datapath + '/network_pretrained_weights'
 
+path_images = datapath + '/images'
+path_images_temp = datapath + '/images/images_aux'
+
+path_dataset = datapath + '/dataset'
