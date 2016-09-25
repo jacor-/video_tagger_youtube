4c881c87eb18f56c229fa6c84eb7fd7b0e417ba8
= <=>
2016-09-25 23:00:09 +0200

    Trying to adapt mnist

--- settings.py
+++ settings.py
@@ -8,6 +8,17 @@ path_images_temp = datapath + '/images/images_aux'
 path_dataset = datapath + '/dataset'
 
 
-youtube_dataset = path_dataset + "/dataset_youtube.csv"
 
+youtube_dataset = path_dataset + "/dataset_youtube.csv"
 youtube_dataset_npy = path_dataset + "/dataset_youtube.npy"
+
+
+
+mnist_seq_dataset_train = path_dataset + "/dataset_mnist_seq_train.csv"
+mnist_seq_dataset_train_npy = path_dataset + "/dataset_mnist_seq_train.npy"
+mnist_seq_dataset_test = path_dataset + "/dataset_mnist_seq_test.csv"
+mnist_seq_dataset_test_npy = path_dataset + "/dataset_mnist_seq_test.npy"
+mnist_seq_dataset_val = path_dataset + "/dataset_mnist_seq_val.csv"
+mnist_seq_dataset_val_npy = path_dataset + "/dataset_mnist_seq_val.npy"
+
+path_mnist_images = datapath + '/images'
